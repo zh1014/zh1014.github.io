3845a05ab28aee5e7f90c51cf181f7e091e6628e
zh1014 <964701944@qq.com>
2019-05-22 17:26:34 +0800

    update blog

---
+++ update.sh
@@ -0,0 +1,18 @@
+#! /bin/sh
+
+# set makes it safer
+set -eux
+set -o pipefail
+
+cd ~/go/src/github.com/zh1014/zh1014.github.io/myblog/
+rm -rf ./public
+hugo
+cd ..
+shopt -s extglob
+git rm -rf ./!(myblog|README.md|.gitignore|.nojekyll|update.sh)
+shopt -u extglob
+mv ./myblog/public/*  .
+git add ./*
+git commit -m "update blog"
+git push origin master:master
+
